d1b900729b7fd90c5c32342d22bb5b852fc1e261
nbrr <nbrr@protonmail.com>
2021-04-02 22:40:15 +0200

    Selection of two endpoints.

--- ta-web-ui/src/components/char.rs
+++ ta-web-ui/src/components/char.rs
@@ -1,6 +1,8 @@
 use boolinator::*;
 use yew::prelude::*;
 
+use super::text;
+
 pub struct CharComponent {
     props: Props,
     link: ComponentLink<Self>,
@@ -11,9 +13,14 @@ pub struct Props {
     pub content: char,
     pub pos: usize,
     pub zones: Vec<usize>,
+    pub parent_text: Callback<text::Msg>,
+    pub selected: bool,
 }
 
-pub enum Msg {}
+pub enum Msg {
+    Click,
+    Hover,
+}
 
 impl Component for CharComponent {
     type Properties = Props;
@@ -24,20 +31,42 @@ impl Component for CharComponent {
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::Click => self
+                .props
+                .parent_text
+                .emit(text::Msg::CharClick(self.props.pos)),
+            Msg::Hover => self
+                .props
+                .parent_text
+                .emit(text::Msg::CharHover(self.props.pos)),
+        }
         true
     }
 
     fn change(&mut self, props: Self::Properties) -> ShouldRender {
+        self.props = props;
         true
     }
 
     fn view(&self) -> Html {
-        let mut classes : Vec<String> = self.props.zones.clone().iter().map(|i| format!("ta-zone-{}",i)).collect();
+        let mut classes: Vec<String> = self
+            .props
+            .zones
+            .clone()
+            .iter()
+            .map(|i| format!("ta-zone-{}", i))
+            .collect();
         classes.push("ta-char".into());
+        if self.props.selected {
+            classes.push("ta-char-selected".into());
+        }
         html! {
             <span
                 class={classes}
                 id={format!("ta-char-pos-{}", self.props.pos)}
+                onclick=self.link.callback(|_| Msg::Click)
+                onmouseover=self.link.callback(|_| Msg::Hover)
             >
                     {self.props.content}
             </span>
--- ta-web-ui/src/components/text.rs
+++ ta-web-ui/src/components/text.rs
@@ -1,8 +1,9 @@
 use boolinator::*;
 use yew::prelude::*;
 
+use ta::{EnrichedText,Interval};
+
 use super::char::*;
-use ta::EnrichedText;
 
 pub struct TextComponent {
     pub props: Props,
@@ -12,9 +13,16 @@ pub struct TextComponent {
 #[derive(PartialEq, Clone, Properties)]
 pub struct Props {
     pub text: EnrichedText<String>,
+    #[prop_or_default]
+    pub ongoing_selection: Option<(usize, usize)>,
+    #[prop_or_default]
+    pub intervals: Vec<Interval>,
 }
 
-pub enum Msg {}
+pub enum Msg {
+    CharClick(usize),
+    CharHover(usize),
+}
 
 impl Component for TextComponent {
     type Properties = Props;
@@ -25,6 +33,23 @@ impl Component for TextComponent {
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::CharClick(pos) => 
+                if let Some((p1,p2)) = self.props.ongoing_selection {
+                    if let Some(i) =Interval::new(p1.min(p2),p2.max(p1)) {
+                      self.props.intervals.push(i);
+                      self.props.ongoing_selection = None
+                    }
+                } else {
+                    self.props.ongoing_selection= Some((pos, pos))
+                },
+            Msg::CharHover(pos) => 
+                self.props.ongoing_selection = self
+                    .props
+                    .ongoing_selection
+                    .and_then(|(p1, _)| Some((p1, pos)))
+            
+        }
         true
     }
 
@@ -45,12 +70,30 @@ impl Component for TextComponent {
                     zones={
                         self.props.text.zones.iter().enumerate().filter_map(|(i, zone)| zone.contains(ind).as_some(i)).collect::<Vec<usize>>()
                     }
+                    selected={self.props.ongoing_selection.filter(|(p1,p2)| p1 <= &ind && p2 >= &ind || p2 <= &ind && p1 >= &ind).is_some()}
+                    parent_text={self.link.callback(|x|x)}
                     />
             })
             .collect::<Html>();
 
+        let mut css : String = "".into();
+
+        for (i,_) in self.props.text.zones.iter().enumerate(){
+             css.push_str(format!(".ta-zone-{} {{ background-color: pink; }}", i).as_str())
+        }
+
+        css.push_str(".ta-char:hover {
+            background-color: red;
+        }
+        
+        .ta-char-selected {
+            background-color: blue;
+        }"
+        .into());
+
         html! {
             <div>
+                <style type="text/css">{css}</style>
                 {chars}
             </div>
         }
